3aab2f2c2ccf0246580229c69dcf34c036fb0ad1
ericb83 <eboerschinger@gmail.com>
2024-07-12 12:37:30 -0500

    initial commit

---
+++ app/components/ui/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Footer = () => {
+    return(
+        <footer className="footer footer-center bg-base-300 text-base-content p-4">
+            <aside>
+                <p>Copyright © {new Date().getFullYear()} - All right reserved by Freedom Homeschool</p>
+            </aside>
+        </footer>
+    )
+}
+
+export default Footer
