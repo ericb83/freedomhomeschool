705b194f240af4882cb79c4a3512a83011b7c599
ericb83 <eboerschinger@gmail.com>
2024-07-14 09:58:58 -0500

    create signup form component and import into signup page

---
+++ app/components/SignUp.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const SignUp = () => {
+
+
+  return (
+   <form action="">
+    <input type="text" placeholder='Name' />
+    <input type="phone" placeholder='Phone Number' />
+    <input type="email" placeholder='Email'/>
+    <input type="password" placeholder='Password'/>
+    <input type="password" placeholder='Confirm Password' />
+
+    <button type='submit'>Sign Up</button>
+
+   </form> 
+)
+}
+
+export default SignUp
