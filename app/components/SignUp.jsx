f3cb3c1631c2abf0719c200d3634242fa62812b9
ericb83 <eboerschinger@gmail.com>
2024-07-18 23:03:24 -0500

    added styles to signup form

--- app/components/SignUp.jsx
+++ app/components/SignUp.jsx
@@ -4,16 +4,29 @@ const SignUp = () => {
 
 
   return (
-   <form action="">
-    <input type="text" placeholder='Name' />
+  <div className='w-full max-w-xs'>
+   <form className='bg-white shadow-lg rounded-xl '  action="">
+    <div>
+    <input className='bg-white shadow-lg rounded-xl px-4 py-4 mt-10 mb-10 ' type="text" placeholder='Name' />
+    </div>
+    <div>
     <input type="phone" placeholder='Phone Number' />
+    </div>
+    <div>
     <input type="email" placeholder='Email'/>
+    </div>
+    <div>
     <input type="password" placeholder='Password'/>
+    </div>
+    <div>
     <input type="password" placeholder='Confirm Password' />
-
+    </div>
+    <div>
     <button type='submit'>Sign Up</button>
+    </div>
 
-   </form> 
+   </form>
+  </div>  
 )
 }
 
--- app/signup/page.js
+++ app/signup/page.js
@@ -2,7 +2,7 @@ import React from "react";
 import SignUp from "../components/SignUp";
 const signupPage = () => {
   return (
-    <div>
+    <div className="flex min-h-screen flex-col items-center justify-center ">
       <h1>Sign Up</h1>
       <SignUp />
     </div>
