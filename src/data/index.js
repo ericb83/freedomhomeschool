ea94a55208c966d3bf0b8472aa6d3403f0ef3d85
ericb83 <eboerschinger@gmail.com>
2024-08-13 19:56:36 -0500

    added data to index.js

--- src/data/index.js
+++ src/data/index.js
@@ -1,5 +1,5 @@
 //initial state
-const initialState = {
+export const initialState = {
   users: [],
   students: [],
   classes: [],
@@ -8,12 +8,45 @@ const initialState = {
 };
 
 //example user
-const users = [
+export const users = [
   {
     id: 1, //user id will be auto generated
     name: "John Doe",
     email: "johndoe@example.com",
     phoneNumber: "123-456-7890",
     password: "hashedpassword", //passwords will be hashed
+  },
+];
+
+//example student
+export const students = [
+  {
+    id: 1,
+    userId: 1, //Associate student with user
+    name: "Jane Doe",
+    address: "123 Main St, Anytown, USA",
+    birthDate: "01-01-1995",
+  },
+];
+
+// example class
+export const classes = [
+  {
+    id: 1,
+    name: "Math 101",
+    subject: "Mathematics",
+    schedule: "MWF 08:00-09:00",
+  },
+];
+
+//example hours
+export const hours = [
+  {
+    id: 1,
+    studentId: 1, //Associate hours with student
+    classId: 1, //Associate hours with class
+    date: "08-13-2024",
+    hours: 2,
+    grade: A,
   },
 ];
